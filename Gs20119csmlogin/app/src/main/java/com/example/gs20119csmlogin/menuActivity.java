d14315ec17781f7b0f7a1e37a97495f5b938efeb
gs20119 <andrew04247@gmail.com>
2021-08-28 20:19:00 +0900

    Practice Inflation in one Activity

--- Gs20119csmlogin/app/src/main/java/com/example/gs20119csmlogin/menuActivity.java
+++ Gs20119csmlogin/app/src/main/java/com/example/gs20119csmlogin/menuActivity.java
@@ -4,24 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class menuActivity extends AppCompatActivity {
-    LinearLayout container; // container = Layout
-
+    LinearLayout container;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu); // 여기까지 기본
+        setContentView(R.layout.activity_menu); // 전체 인플레이션, menu XML 파일 보기
 
-        container = findViewById(R.id.container);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener((view) -> {
+        // XML의 요소를 Activity에 가져오고 싶으면 findView
+        container = findViewById(R.id.container); // 전체 레이아웃 (id container)
+        Button button = findViewById(R.id.button); // 버튼 (id button)
+        button.setOnClickListener((view) -> { // button + onClick
             LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            inflater.inflate(R.layout.sub1, container, attachTo);
-        }); // Layout Inflater 기본 사용법
+            inflater.inflate(R.layout.sub1, container, true);
+            // 부분 인플레이션은 이렇게 inflater을 하나 만들어서 사용함, sub1 XML 파일 붙이기
+        });
     }
 }
