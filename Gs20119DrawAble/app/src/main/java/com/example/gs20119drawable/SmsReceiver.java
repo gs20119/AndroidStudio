3c8f24ec3381135220198be01736b1a86a50bbbb
gs20119 <andrew04247@gmail.com>
2021-09-14 20:20:19 +0900

    Merge SMS Receiver + Drawable

---
+++ Gs20119DrawAble/app/src/main/java/com/example/gs20119drawable/SmsReceiver.java
@@ -0,0 +1,70 @@
+package com.example.gs20119drawable;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.example.gs20119drawable.MainActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG = "SmsReceiver";
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(SMS_RECEIVED))
+        {
+            Log.d(TAG, "onReceiver() 호출");
+            Bundle bundle = intent.getExtras(); SmsMessage[] messages = parseSmsMessage(bundle);
+            if (messages.length > 0)
+            {
+                String sender = messages[0].getOriginatingAddress();
+                String contents = messages[0].getMessageBody().toString();
+                Date receivedDate = new Date(messages[0].getTimestampMillis());
+
+                Log.d(TAG, "Sender :" + sender);
+                Log.d(TAG, "contents : " + contents);
+                Log.d(TAG, "receivedDate : " + receivedDate);
+                sendToActivity(context, sender, contents, receivedDate);
+            }
+        }
+    }
+
+    private void sendToActivity(Context context, String sender, String contents, Date receivedDate){
+        Intent intent = new Intent(context, MainActivity.class); // Flag 설정
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP); // 메세지의 내용을 Extra에 넣어줌
+        intent.putExtra("sender", sender);
+        intent.putExtra("contents", contents);
+        intent.putExtra("receivedDate", format.format(receivedDate));
+        context.startActivity(intent);
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle)
+    {
+        Object[] objs = (Object[]) bundle.get("pdus");
+        SmsMessage[] messages = new SmsMessage[objs.length];
+        for(int i=0; i<objs.length; i++)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+            }
+            else
+            {
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+            }
+        }
+        return messages;
+    }
+}
